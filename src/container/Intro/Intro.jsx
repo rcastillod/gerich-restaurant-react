3fd2c7f9db244d5f687ce2b2c9ce7316196ce6e0
rcastillod <rodrigo.castillo@global3.cl>
2023-04-19 18:09:51 -0400

    feat: :sparkles: Video section ready

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,50 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 
-import './Intro.css';
+// Icons
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+// Assets
+import { meal } from "../../constants";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+import "./Intro.css";
+
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const videoRef = useRef();
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if (playVideo) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+  };
+
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={videoRef}
+        type="video/mp4"
+        loop
+        control={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#fff" fontSize={40} />
+          ) : (
+            <BsFillPlayFill color="#fff" fontSize={40} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
