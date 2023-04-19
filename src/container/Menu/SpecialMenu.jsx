3dffe4f104eae2801c8d2f5bb982a5046ab332fd
rcastillod <rodrigo.castillo@global3.cl>
2023-04-19 10:26:21 -0400

    feat: :lipstick: Add body bg pattern

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
+// Components
+import { SubHeading, MenuItem } from "../../components";
+// Assets
+import { images, data } from "../../constants";
+
 import "./SpecialMenu.css";
 
-// Assets
-import images from "../../constants";
-
-const SpecialMenu = () => <div>SpecialMenu</div>;
+const SpecialMenu = () => (
+  <div id="menu" className="app__specialMenu section__padding-block">
+    <div className="container">
+      <div className="flex__center">
+        <div className="app__specialMenu-title">
+          <SubHeading title="Menu that fits you palatte" />
+          <h3 className="headtext__cormorant">Today’s Special</h3>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default SpecialMenu;
